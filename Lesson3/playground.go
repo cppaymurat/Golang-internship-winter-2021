6b6463159e88b626645fd69dec8df016212baef7
cppaymurat <cppaymurat@yandex.ru>
2021-01-27 14:12:36 +0600

    Pushing from Goland

---
+++ Lesson3/playground.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	botToken := "bot1538871626:AAFOsOEgv2jiLOHKTsYrox2SAp34Q7w37y0"
+	botAPI := "https://api.telegram.org/"
+	botURL := botAPI + botToken
+	offset := 0
+	for {
+		updates, err := getUpdates(botURL, offset)
+		if err != nil {
+			panic(err)
+		}
+		for _, update := range updates {
+			err = respond(botURL, update)
+			if err != nil {
+				panic(err)
+			}
+			offset = update.Update_id + 1
+		}
+	}
+}
+
+func getUpdates(botURL string, offset int) ([]Update, error) {
+	allSource, err := http.Get(botURL + "/getUpdates?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer allSource.Body.Close()
+	body, err_ := ioutil.ReadAll(allSource.Body)
+	if err_ != nil {
+		return nil, err_
+	}
+	result := Respond{}
+	_err := json.Unmarshal(body, &result)
+	if _err != nil {
+		return nil, _err
+	}
+	return result.Update, _err
+}
+
+func respond(botURL string, update Update) error {
+	currentMessage := BotMessage{}
+	update.Message.Text = strings.ToLower(update.Message.Text)
+	if update.Message.Text == "привет" {
+		currentMessage.Text = "И тебе привет!"
+	} else if update.Message.Text == "как тебя зовут?" {
+		currentMessage.Text = "Меня звать Алан! Я Чеченец!"
+	} else if update.Message.Text == "как зовут твою жену?" {
+		currentMessage.Text = "Ее тоже звать Алан! Она тоже чеченец!"
+	} else {
+		currentMessage.Text = "Бот не человек, он не понимает такое..."
+	}
+	currentMessage.ChatID = update.Message.Chat.ChatID
+	buffer, err := json.Marshal(currentMessage)
+	if err != nil {
+		return err
+	}
+	_, err = http.Post(botURL+"/sendMessage", "application/json", bytes.NewBuffer(buffer))
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ Lesson3/structures.go
@@ -0,0 +1,40 @@
+package main
+
+type GetMeResult struct {
+	Id                          int    `json:"id"`
+	Is_bot                      bool   `json:"is_bot"`
+	First_name                  string `json:"first_name"`
+	Username                    string `json:"username"`
+	Can_join_groups             bool   `json:"can_join_groups"`
+	Can_read_all_group_messages bool   `json:"can_read_all_group_messages"`
+	Supports_inline_queries     bool   `json:"supports_inline_queries"`
+}
+
+type GetMeResponse struct {
+	Ok     bool        `json:"ok"`
+	Result GetMeResult `json:"result"`
+}
+
+type Update struct {
+	Update_id int     `json:"update_id"`
+	Message   Message `json:"message"`
+}
+
+type Message struct {
+	Chat Chat `json:"chat"`
+	//Message_id int `json:"message_id"`
+	Text string `json:"text"`
+}
+
+type Chat struct {
+	ChatID int `json:"id"`
+}
+
+type Respond struct {
+	Update []Update `json:"result"`
+}
+
+type BotMessage struct {
+	ChatID int    `json:"chat_id"`
+	Text   string `json:"text"`
+}
